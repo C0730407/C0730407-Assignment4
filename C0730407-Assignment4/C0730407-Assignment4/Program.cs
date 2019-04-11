7d6759616bd1f6296621aba59def3fb47a9e2455
visualstudio <visualstudio@230-PC07>
2019-04-11 16:23:01 -0400

    Section C

--- C0730407-Assignment4/C0730407-Assignment4/Program.cs
+++ C0730407-Assignment4/C0730407-Assignment4/Program.cs
@@ -1,4 +1,4 @@
-﻿//Section BB
+﻿//Section C
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,52 +20,23 @@ namespace Assignment_4
             p.Run();
         }
 
-        public void Run() { this.ReadTextFiles(); }
+        public void Run() { this.Wordfind(); }
 
-        public void ReadTextFiles()
+        public void Wordfind()
+
         {
-            
-
-                StreamReader reader = new StreamReader("U:/Users/730407/github/Beowulf.txt");
-                string script = reader.ReadToEnd();
-
-                
-                var text = script.Trim();
-                int Count = 0, index = 0;
-
-               
-                while (index < text.Length)
+            int chr = 0;
+            foreach (var line in File.ReadAllLines("U:/Users/730407/github/Beowulf.txt"))
+            {
+                if (line.Contains("sea") && line.Contains("fare"))
                 {
-
-                    while (index < text.Length && !char.IsWhiteSpace(text[index]))
-                        index++;
-
-                   
-                    Count++;
-
-                    while (index < text.Length && char.IsWhiteSpace(text[index]))
-                        index++;
+                    chr++;
                 }
 
-                Console.WriteLine("Total Number of Words are " + Count);
-
+                
             }
-        
-
-            public int FindNumberOfBlankSpaces(string line)
-        {
-            //https://stackoverflow.com/questions/17812566/count-words-and-spaces-in-string-c-sharp
-            int countletters = 0;
-            int countSpaces = 0;
-
-            foreach (char c in line)
-            {
-                if (char.IsLetter(c)) { countletters++; }
-                if (char.IsWhiteSpace(c)) { countSpaces++; }
-            }
-            return countSpaces;
-
-
+            Console.WriteLine("Total words sea and fare in File are:" + chr);
         }
     }
-}
+} 
+               
